93185f541ba98702e0f9360c2c19e155e8d7306d
blackier <fengxiyulei@sina.com>
2023-05-27 23:29:02 +0800

    refactor: 项目结构小调整

--- BKTrans/Behaviors/DataGridBehavior.cs
+++ BKTrans/Behaviors/DataGridBehavior.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xaml.Behaviors;
+﻿using BKTrans.Misc;
+using Microsoft.Xaml.Behaviors;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,132 +12,9 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 
-// https://stackoverflow.com/questions/10493450/drag-drop-row-behavior-on-wpf-datagrid
 namespace BKTrans
 {
-    public static class UIHelpers
-    {
-
-        #region find parent
-
-        /// <summary>
-        /// Finds a parent of a given item on the visual tree.
-        /// </summary>
-        /// <typeparam name="T">The type of the queried item.</typeparam>
-        /// <param name="child">A direct or indirect child of the
-        /// queried item.</param>
-        /// <returns>The first parent item that matches the submitted
-        /// type parameter. If not matching item can be found, a null
-        /// reference is being returned.</returns>
-        public static T TryFindParent<T>(DependencyObject child)
-          where T : DependencyObject
-        {
-            //get parent item
-            DependencyObject parentObject = GetParentObject(child);
-
-            //we've reached the end of the tree
-            if (parentObject == null) return null;
-
-            //check if the parent matches the type we're looking for
-            T parent = parentObject as T;
-            if (parent != null)
-            {
-                return parent;
-            }
-            else
-            {
-                //use recursion to proceed with next level
-                return TryFindParent<T>(parentObject);
-            }
-        }
-
-
-        /// <summary>
-        /// This method is an alternative to WPF's
-        /// <see cref="VisualTreeHelper.GetParent"/> method, which also
-        /// supports content elements. Do note, that for content element,
-        /// this method falls back to the logical tree of the element.
-        /// </summary>
-        /// <param name="child">The item to be processed.</param>
-        /// <returns>The submitted item's parent, if available. Otherwise
-        /// null.</returns>
-        public static DependencyObject GetParentObject(DependencyObject child)
-        {
-            if (child == null) return null;
-            ContentElement contentElement = child as ContentElement;
-
-            if (contentElement != null)
-            {
-                DependencyObject parent = ContentOperations.GetParent(contentElement);
-                if (parent != null) return parent;
-
-                FrameworkContentElement fce = contentElement as FrameworkContentElement;
-                return fce != null ? fce.Parent : null;
-            }
-
-            //if it's not a ContentElement, rely on VisualTreeHelper
-            return VisualTreeHelper.GetParent(child);
-        }
-
-        #endregion
-
-
-        #region update binding sources
-
-        /// <summary>
-        /// Recursively processes a given dependency object and all its
-        /// children, and updates sources of all objects that use a
-        /// binding expression on a given property.
-        /// </summary>
-        /// <param name="obj">The dependency object that marks a starting
-        /// point. This could be a dialog window or a panel control that
-        /// hosts bound controls.</param>
-        /// <param name="properties">The properties to be updated if
-        /// <paramref name="obj"/> or one of its childs provide it along
-        /// with a binding expression.</param>
-        public static void UpdateBindingSources(DependencyObject obj,
-                                  params DependencyProperty[] properties)
-        {
-            foreach (DependencyProperty depProperty in properties)
-            {
-                //check whether the submitted object provides a bound property
-                //that matches the property parameters
-                BindingExpression be = BindingOperations.GetBindingExpression(obj, depProperty);
-                if (be != null) be.UpdateSource();
-            }
-
-            int count = VisualTreeHelper.GetChildrenCount(obj);
-            for (int i = 0; i < count; i++)
-            {
-                //process child items recursively
-                DependencyObject childObject = VisualTreeHelper.GetChild(obj, i);
-                UpdateBindingSources(childObject, properties);
-            }
-        }
-
-        #endregion
-
-
-        /// <summary>
-        /// Tries to locate a given item within the visual tree,
-        /// starting with the dependency object at a given position. 
-        /// </summary>
-        /// <typeparam name="T">The type of the element to be found
-        /// on the visual tree of the element at the given location.</typeparam>
-        /// <param name="reference">The main element which is used to perform
-        /// hit testing.</param>
-        /// <param name="point">The position to be evaluated on the origin.</param>
-        public static T TryFindFromPoint<T>(UIElement reference, Point point)
-          where T : DependencyObject
-        {
-            DependencyObject element = reference.InputHitTest(point)
-                                         as DependencyObject;
-            if (element == null) return null;
-            else if (element is T) return (T)element;
-            else return TryFindParent<T>(element);
-        }
-    }
-
+    // https://stackoverflow.com/questions/10493450/drag-drop-row-behavior-on-wpf-datagrid
     public class DataGridBehavior : Behavior<DataGrid>
     {
         private object draggedItem;
@@ -221,7 +99,7 @@ namespace BKTrans
         {
             if (isEditing) return;
 
-            var row = UIHelpers.TryFindFromPoint<DataGridRow>((UIElement)sender, e.GetPosition(AssociatedObject));
+            var row = BKUIHelpers.TryFindFromPoint<DataGridRow>((UIElement)sender, e.GetPosition(AssociatedObject));
             if (row == null || row.IsEditing || row.DataContext == CollectionView.NewItemPlaceholder) return;
 
             //set flag that indicates we're capturing mouse movements
@@ -289,7 +167,7 @@ namespace BKTrans
 
             //make sure the row under the grid is being selected
             var position = e.GetPosition(AssociatedObject);
-            var row = UIHelpers.TryFindFromPoint<DataGridRow>(AssociatedObject, position);
+            var row = BKUIHelpers.TryFindFromPoint<DataGridRow>(AssociatedObject, position);
             if (row != null) AssociatedObject.SelectedItem = row.Item;
         }
     }
